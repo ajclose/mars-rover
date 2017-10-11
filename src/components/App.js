318f86c007d1b6f20aa4537620cd07d3f89d5632
Alex Close <ajclose@gmail.com>
2017-10-11 13:57:06 -0500

    Create GetImageForm component and hook up to api call

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 
+import GetImageForm from './getimageform'
+
 class App extends Component {
   render() {
     return (
-      <div className="App">
-
+      <div>
+        <GetImageForm />
       </div>
     );
   }
---
+++ src/components/getimageform.js
@@ -0,0 +1,67 @@
+import React, { Component } from 'react'
+const API_KEY = "FPKJxnDFMAQE5rpVavzMktJFAzlsXFlLzI4sfi4c"
+
+export default class GetImageForm extends Component {
+  constructor(props) {
+    super(props)
+
+    this.handleRover = this.handleRover.bind(this)
+    this.handleCamera = this.handleCamera.bind(this)
+    this.handleSol = this.handleSol.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+
+    this.state = {
+      rover: "Curiosity",
+      camera: "fhaz",
+      sol: "",
+      images: []
+    }
+  }
+
+  handleRover(e) {
+    this.setState({rover: e.target.value})
+  }
+
+  handleCamera(e) {
+    this.setState({camera: e.target.value})
+  }
+
+  handleSol(e) {
+    this.setState({sol: e.target.value})
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    this.setState({camera: this.state.camera, rover: this.state.rover, sol: this.state.sol})
+    let camera = this.state.camera
+    let rover = this.state.rover
+    let sol = this.state.sol
+    let imageUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&camera=${camera}&api_key=${API_KEY}`
+
+    fetch(imageUrl)
+    .then(response => response.json())
+    .then(json => console.log(json))
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="rover">Rover</label>
+        <select id="rover" value={this.state.rover} onChange={this.handleRover}>
+          <option value="curiosity">Curiosity</option>
+          <option value="opportunity">Opportunity</option>
+          <option value="spirit">Spirit</option>
+        </select>
+        <label htmlFor="camera">Camera</label>
+        <select id="camera" value={this.state.camera} onChange={this.handleCamera}>
+          <option value="fhaz">FHAZ (Front Hazard)</option>
+          <option value="rhaz">RHAZ (Rear Hazard)</option>
+          <option value="navcam">NAVCAM (Navigation Cam)</option>
+        </select>
+        <label htmlFor="sol">Martian Sol: 1000-2000</label>
+        <input type="number" onChange={this.handleSol} max="2000" min="1000" value={this.state.sol}/>
+        <button type="submit">Get Rover Image</button>
+      </form>
+    )
+  }
+}
